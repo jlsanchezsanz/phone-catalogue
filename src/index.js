c8def60b34245a0da915338ba88da53d96e014f5
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-06-05 18:13:20 +0200

    feat(styles): add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { store } from './state/store';
 import './index.css';
