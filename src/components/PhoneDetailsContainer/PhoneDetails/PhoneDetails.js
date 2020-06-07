122ecbbeafb6da354afc3a604d1d8210c467b4d3
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-06-07 12:10:57 +0200

    styling(PhoneDetails): apply styles to phone details

--- src/components/PhoneDetailsContainer/PhoneDetails/PhoneDetails.js
+++ src/components/PhoneDetailsContainer/PhoneDetails/PhoneDetails.js
@@ -3,39 +3,59 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { PhonePropType } from '../../../prop-types/phone.prop-type';
+import './PhoneDetails.css';
 
 function PhoneDetails(props) {
   return (
-    <div>
+    <div className='details'>
       <Link to='/phones'>All phones</Link>
-      <img
-        className='image'
-        src={require(`../../../assets/${props.phone.imageFileName}`)}
-        alt={props.phone.name}
-      />
-      <h1 className='display-4'>
-        {props.phone.manufacturer} {props.phone.name}
-      </h1>
-      <ul className='traits'>
-        <li className='traits__item'>
-          <span>{props.phone.price}€</span>
-        </li>
-        <li className='traits__item'>
-          <span>{props.phone.color}</span>
-        </li>
-        <li className='traits__item'>
-          <span>{props.phone.screen}</span>
-        </li>
-        <li className='traits__item'>
-          <span>{props.phone.processor}</span>
-        </li>
-        <li className='traits__item'>
-          <span>{props.phone.ram}GB RAM</span>
-        </li>
-        <li className='traits__item'>
-          <span>{props.phone.description}</span>
-        </li>
-      </ul>
+      <div className='details__wrapper'>
+        <div className='details__img-gallery'>
+          <img
+            className='image'
+            src={require(`../../../assets/${props.phone.imageFileName}`)}
+            alt={props.phone.name}
+          />
+        </div>
+        <div className='details__info'>
+          <h1>
+            {props.phone.manufacturer} {props.phone.name} {props.phone.color}
+          </h1>
+          <div className='info__price'>{props.phone.price} €</div>
+          <button type='button' class='btn btn-info'>
+            Add to cart
+          </button>
+        </div>
+      </div>
+      <div className='details__wrapper details__specifications'>
+        <h4>SPECS</h4>
+        <ul className='list-group'>
+          <li className='list-group-item' key='screen'>
+            <dl>
+              <dt>Screen:</dt>
+              <dd>{props.phone.screen}</dd>
+            </dl>
+          </li>
+          <li className='list-group-item' key='processor'>
+            <dl>
+              <dt>Processor:</dt>
+              <dd>{props.phone.processor}</dd>
+            </dl>
+          </li>
+          <li className='list-group-item' key='ram'>
+            <dl>
+              <dt>Memory:</dt>
+              <dd>{props.phone.ram}GB RAM</dd>
+            </dl>
+          </li>
+          <li className='list-group-item' key='description'>
+            <dl>
+              <dt>Description:</dt>
+              <dd>{props.phone.description}</dd>
+            </dl>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
