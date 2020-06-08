269ff84388b0918658cebd699e1c784572016209
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-06-08 20:16:52 +0200

    feat(PhonesList): filter by manufacturer

--- src/components/PhonesListContainer/PhonesList/PhonesList.js
+++ src/components/PhonesListContainer/PhonesList/PhonesList.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import Filters from '../../Filters';
 import PhonesListItem from './PhonesListItem';
 import { PhonePropType } from '../../../prop-types/phone.prop-type';
 import './PhonesList.css';
+import { MANUFACTURERS } from '../../../constants/manufacturers';
 
-export default function PhonesList(props) {
+export default function PhonesList({ phones }) {
+  const [filteredPhones, setFilteredPhones] = useState(phones);
+
+  function handleFiltersChange(filters) {
+    const { manufacturer } = filters;
+    const filteredPhones =
+      manufacturer === MANUFACTURERS.All
+        ? phones
+        : phones.filter((phone) => phone.manufacturer === manufacturer);
+    setFilteredPhones(filteredPhones);
+  }
+
   return (
-    <div className='card-deck'>
-      {props.phones.map((phone) => (
-        <Link to={`/phones/${phone.id}`} key={phone.id}>
-          <PhonesListItem phone={phone} />
-        </Link>
-      ))}
-    </div>
+    <>
+      <Filters onFiltersChange={handleFiltersChange} />
+      <div className='card-deck'>
+        {filteredPhones.map((phone) => (
+          <Link to={`/phones/${phone.id}`} key={phone.id}>
+            <PhonesListItem phone={phone} />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/PhonesListContainer/PhonesList/__tests__/PhonesList.test.js
+++ src/components/PhonesListContainer/PhonesList/__tests__/PhonesList.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import PhonesList from '../PhonesList';
 import { phonesMock } from '../../../../mocks/phones.mock';
+import { MANUFACTURERS } from '../../../../constants/manufacturers';
 
 const setUpShallow = (phones) => {
   const component = shallow(<PhonesList phones={phones} />);
@@ -14,6 +15,24 @@ describe('PhonesList', () => {
 
   it('should display phones list', () => {
     component = setUpShallow(phonesMock.data);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should show all phones', () => {
+    component = setUpShallow(phonesMock.data);
+    const manufacturersSelect = component.find('Filters');
+    manufacturersSelect.prop('onFiltersChange')({
+      manufacturer: MANUFACTURERS.All
+    });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should filter phones by manufacterer', () => {
+    component = setUpShallow(phonesMock.data);
+    const manufacturersSelect = component.find('Filters');
+    manufacturersSelect.prop('onFiltersChange')({
+      manufacturer: MANUFACTURERS.Apple
+    });
     expect(component).toMatchSnapshot();
   });
 });
