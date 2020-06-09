186308be2c76dfb41981ea2717d8bbee462f2528
sanchezjl <sanchezjl@ryanair.com>
2020-06-09 17:22:38 +0200

    chore(ManufacturerSelect): add package.json

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ManufacturerSelect from './ManufacturerSelect/ManufacturerSelect';
+import ManufacturerSelect from './ManufacturerSelect';
 
 export default function Filters({ onFiltersChange }) {
   return (
